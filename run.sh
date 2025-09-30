aae592c388adac28a2830331bae350ce8fb95037
Louis Roché (Ahrefs) <louis.roche@ahrefs.com>
2025-09-30 07:57:42 +0000

    run.sh: build in release mode and fix re call

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo "pcre2"
-dune exec bin/pcre2.exe $1
+dune exec --profile=release bin/pcre2.exe $1
 echo "pcre"
-dune exec bin/pcre.exe $1
+dune exec --profile=release bin/pcre.exe $1
 echo "re"
-dune exec bin/re2.exe $1
+dune exec --profile=release bin/re.exe $1
 echo "re2"
-dune exec bin/re2.exe $1
+dune exec --profile=release bin/re2.exe $1
